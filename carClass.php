e0e1558cf9b8da1461a01ab69b26148d60be074e
Gjgordy <gjgordy1996@yahoo.com>
2016-04-29 09:53:24 -0600

    carClass.php object oriented

---
+++ carClass.php
@@ -0,0 +1,69 @@
+<?php
+
+class car {
+
+ private $color;
+ private $make;
+ private $name;
+
+ public function __construct ($newColor, $newMake, $newName) {
+   $this->setColor($newColor);
+   $this->setMake($newMake);
+   $this->setName($newName);
+ }
+
+ public function setColor($newColor) {
+       $this->color = $newColor;
+     return;
+        }
+
+ public function setMake($newMake) {
+       $this->make = $newMake;
+     return;
+        }
+
+   public function setName($newName) {
+         $this->name = $newName;
+       return;
+      }
+
+   public function getColor() {
+            return ($this->color);
+    }
+
+ public function getMake() {
+     return ($this->make);
+        }
+
+ public function getName() {
+     return ($this->name);
+ }
+
+
+
+ public function go() {
+   echo  $this->name . "car going";
+   return;
+ }
+
+ public function stop() {
+   echo $this->name . "car stopping";
+ }
+
+}
+$fCar = new Car ("blue", "ford", "fCar");
+$toyCar = new Car ("red", "toyota", "toyCar");
+
+echo $fCar->getColor() . "<br>";
+
+echo $toyCar->getColor() . "<br>";
+
+echo $fCar->go() . "<br>";
+
+echo $toyCar->go() . "<br>";
+
+echo $toyCar->stop() . "<br>";
+
+echo $fCar->stop() . "<br>";
+
+echo $toyCar->getName() . "<br>";
